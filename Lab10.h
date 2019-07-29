636d993b27bca558e3b600aff4d1a8847ee3dba0
Jokex3 <kennymjose@gmail.com>
2019-07-28 22:58:56 -0400

    Cambios agregando funciones

--- Lab10.h
+++ Lab10.h
@@ -1,4 +1,122 @@
+#ifndef stdio_h
+#include <stdio.h>
+#define stdio_h
+#endif
+
+#ifndef stdlib_h
+#include <stdlib.h>
+#define stdlib_h
+#endif
+
+#ifndef time_h
+#include <time.h>
+#define time_h
+#endif
+
+#ifndef string_h
+#include "string.h"
+#define string_h
+#endif
+
+#ifndef math_h
+#include "math.h"
+#define math_h
+#endif
+
 typedef struct Tnodo{
     char *palabra;
-     
+    int totalDePalabras;
 }Nodo;
+
+int operacionHash(char *palabra,int tamHash){
+    if(palabra == NULL){
+        return -1;
+    }
+    unsigned int radix = 0;
+    int cont=0;
+    while((palabra[cont]!='\0')&&(cont<6)){(
+        radix = radix + letraAnum(palabra[cont]*pow(27,cont)));
+        cont++;
+    }
+    radix = radix%tamHash;
+    return (radix);
+}
+
+int letraAnum(char letra){
+    if(letra == 'ñ'){
+        return 14;
+    }
+    if(letra == 'o'){
+        letra - 'a';
+    }
+    letra = (letra - 'a') + 1;
+    return letra;
+}
+
+int operacionHash2(char *palabra,int tamHash){
+    if(palabra == NULL){
+        return -1;
+    }
+    unsigned int radix = 0;
+    int cont=0;
+    while((palabra[cont]!='\0')&&(cont<6)){(
+        radix = radix + letraAnum(palabra[cont]*pow(27,cont)));
+        cont++;
+    }
+    radix = 1+(radix%tamHash-1);
+    return (radix);
+}
+
+void iniciarTabla(Nodo *tablaHash[],int tamTabla){
+    for(int cont=0; cont<tamTabla; cont++){
+        tablaHash[cont] = 0;
+    }
+}
+
+int cantidadDePalabrasArchivo(char *nombreArchivo){
+    strcat(nombreArchivo,".txt");
+    FILE *archivo = fopen(nombreArchivo,"r");
+    int contador=0;
+    char string[30];
+    fgets(string,30,archivo);
+    while(!feof(archivo)){
+        for(int i=0;i<strlen(string);i++){
+            if(string[i] == '\n'){
+                contador++;
+            }
+            contador++;
+        }
+    }
+    contador = contador+1;
+
+    fclose(archivo);
+    return contador;
+}
+
+void insertarPalabra(Nodo *tablaHash[],char *palabra,int tamTabla){
+    int resultado = operacionHash(palabra);
+    int resultado2 = operacionHash2(palabra);
+}
+
+int leerArchivo(Nodo* tablaHash[],char *nombre, int tamTabla){
+   strcat(nombre,".txt"); //Agrega .txt despues del ultimo caracter del string
+    char *palabraExtraida;
+    int totalDePalabras = 0;
+    FILE *archivo = fopen(nombre,"r");
+    while(!feof(archivo)){ //Mientras no sea final de archivo
+        palabraExtraida = (char *)malloc(sizeof(char)*30);
+        fscanf(archivo,"%s",palabraExtraida);//Copia el string del archivo y la deja en el string palabraextraida
+        insertarPalabra(tablaHash,palabraExtraida,tamTabla);//Ingresa la palabra al nuevo nodo
+        totalDePalabras++;
+        free(palabraExtraida);
+    }
+    fclose(archivo);
+    tablaHash[0]->totalDePalabras = totalDePalabras; //Se guarda el total de palabras en la estructura, pero solo en la primera posicion
+    return totalDePalabras;
+}
+
+int creaTablaHash(char *nombreArchivo,int porcentajeDeUso){
+    int tamHash = cantidadDePalabrasArchivo(nombreArchivo);
+    tamHash = tamHash*porcentajeDeUso;
+    return tamHash;
+}
--- MainLab10.c
+++ MainLab10.c
@@ -1,7 +1,47 @@
+#ifndef stdio_h
 #include <stdio.h>
+#define stdio_h
+#endif
+
+#ifndef stdlib_h
+#include <stdlib.h>
+#define stdlib_h
+#endif
+
+#ifndef time_h
+#include <time.h>
+#define time_h
+#endif
+
+#ifndef string_h
+#include "string.h"
+#define string_h
+#endif
+
+#ifndef math_h
+#include "math.h"
+#define math_h
+#endif
+
+#ifndef Lab10_h
+#include "Lab10.h"
+#define Lab10_h
+#endif
+
 
 int main(){
+    char *nombreArchivo;
+    nombreArchivo = (char *)malloc(sizeof(char)*50);
+    int porcentajeDeUso;
+    printf("Ingrese el nombre del archivo: ");
+    scanf("%s",nombreArchivo);
+    printf("\nIngrese el porcentaje que usar%c: ",160);
+    scanf("%i",&porcentajeDeUso);
 
+    int tamHash = creaTablaHash(nombreArchivo,porcentajeDeUso);
+
+    Nodo *tablaHash[tamHash];
+    iniciarTabla(tablaHash,tamHash);
 
     return 0;    
 }
